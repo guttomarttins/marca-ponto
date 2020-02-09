f41e1bbcb3f0a3018e582bce1ddf70420b6e316e
Carlos Martins <guttomarttins@gmail.com>
2020-02-09 11:22:16 -0300

    modulo login mapeado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material';
-import { LoginModule } from './autenticacao/login/login.module';
+import { LoginModule } from './autenticacao';
 
 @NgModule({
   declarations: [
