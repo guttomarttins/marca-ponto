2f9d5532ac1e5982abc87f8bd5d6145f3495b132
Carlos Martins <guttomarttins@gmail.com>
2020-02-13 23:18:29 -0300

    add tela cadastro pj

--- src/app/autenticacao/cadastro-pj/cadastro-pj.module.ts
+++ src/app/autenticacao/cadastro-pj/cadastro-pj.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CadastrarPjComponent, CadastroPjComponent } from './components';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { RouterModule } from '@angular/router';
+
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { CadastrarPjComponent, CadastroPjComponent } from './components';
+import { MatInputModule, MatButtonModule, MatListModule, MatIconModule, MatTooltipModule, MatSnackBarModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatListModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatSnackBarModule
   ],
   declarations: [
     CadastrarPjComponent,
