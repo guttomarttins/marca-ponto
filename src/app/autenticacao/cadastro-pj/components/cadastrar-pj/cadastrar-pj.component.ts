97160b36c94b105134fa2c9ba7c2bcb96709b93d
Carlos Martins <guttomarttins@gmail.com>
2020-02-15 14:38:30 -0300

    Add cpf validator

--- src/app/autenticacao/cadastro-pj/components/cadastrar-pj/cadastrar-pj.component.ts
+++ src/app/autenticacao/cadastro-pj/components/cadastrar-pj/cadastrar-pj.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material';
 import { Router } from '@angular/router';
 import { CadastroPj } from '../../models';
+import { CpfValidator } from 'src/app/shared/validators';
 
 @Component({
   selector: 'app-cadastrar-pj',
@@ -27,7 +28,7 @@ export class CadastrarPjComponent implements OnInit {
       nome: ['', [Validators.required, Validators.minLength(3)]],
       email: ['', [Validators.required, Validators.email]],
       senha: ['', [Validators.required, Validators.minLength(6)]],
-      cpf: ['', [Validators.required]],
+      cpf: ['', [Validators.required, CpfValidator]],
       razaoSocial: ['', [Validators.required, Validators.minLength(5)]],
       cnpj: ['', [Validators.required]]
     });
