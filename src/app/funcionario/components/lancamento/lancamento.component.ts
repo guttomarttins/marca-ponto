9bb52caac3a33dea551b1fa1278fde255ee3b73a
Carlos Martins <guttomarttins@gmail.com>
2020-02-16 12:14:23 -0300

    Add tela de lancamento de ponto

--- src/app/funcionario/components/lancamento/lancamento.component.ts
+++ src/app/funcionario/components/lancamento/lancamento.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material';
+
+import * as moment from 'moment';
+import { Router } from '@angular/router';
+import { Tipo } from 'src/app/shared';
+
+declare var navigator: any;
 
 @Component({
   selector: 'app-lancamento',
@@ -7,9 +14,77 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LancamentoComponent implements OnInit {
 
-  constructor() { }
+  private dataAtualEn: string;
+  dataAtual: string;
+  geoLocation: string;
+  ultimoTipoLancamento: string;
+
+  constructor(
+    private snackBar: MatSnackBar,
+    private router: Router) { }
 
   ngOnInit() {
+     this.dataAtual = moment().format('DD/MM/YYYY HH:mm:ss');
+     this.dataAtualEn = moment().format('YYYY-MM-DD HH:mm:ss');
+     this.obterGeoLocation();
+     this.ultimoTipoLancamento = '';
+     this.obterUltimoLancamento();
+  }
+
+  obterGeoLocation(): string {
+    if(navigator.geolocation){
+       navigator.geolocation.getCurrentPosition(position =>
+          this.geoLocation = `${position.coords.latitude},${position.coords.longitude}`);
+    }
+    return '';
+  }
+
+  iniciarTrabalho() {
+    this.cadastrar(Tipo.INICIO_TRABALHO);
+  }
+
+  terminarTrabalho() {
+    this.cadastrar(Tipo.TERMINO_TRABALHO);
+  }
+
+  iniciarAlmoco() {
+    this.cadastrar(Tipo.INICIO_ALMOCO);
+  }
+
+  terminarAlmoco() {
+    this.cadastrar(Tipo.TERMINO_ALMOCO);
+  }
+  
+  obterUltimoLancamento() {
+    this.ultimoTipoLancamento = '';
+  }
+  
+  cadastrar(tipo: Tipo) {
+    alert(`Tipo: ${tipo}, dataAtualEn: ${this.dataAtualEn},
+       geolocation: ${this.geoLocation}`);
+  }
+
+  obterUrlMapa(): string{
+    return "https://www.google.com/maps/search/?api=1&query=" +
+        this.geoLocation;
+  }
+  
+  exibirInicioTrabalho(): boolean{
+    return this.ultimoTipoLancamento == "" ||
+       this.ultimoTipoLancamento == Tipo.TERMINO_TRABALHO;
+  }
+
+  exibirTerminoTrabalho(): boolean{
+    return this.ultimoTipoLancamento == Tipo.INICIO_TRABALHO ||
+       this.ultimoTipoLancamento == Tipo.TERMINO_ALMOCO;
+  }
+
+  exibirInicioAlmoco(): boolean{
+    return this.ultimoTipoLancamento == Tipo.INICIO_TRABALHO;
+  }
+
+  exibirTerminoAlmoco(): boolean{
+    return this.ultimoTipoLancamento == Tipo.TERMINO_ALMOCO;
   }
 
 }
--- src/app/funcionario/funcionario.module.ts
+++ src/app/funcionario/funcionario.module.ts
@@ -3,12 +3,25 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FuncionarioComponent, LancamentoComponent, ListagemComponent } from './components';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule, MatListModule, MatTooltipModule, MatIconModule, MatButtonModule, MatSnackBarModule, MatTableModule, MatPaginatorModule, MatSortModule, MatCardModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatListModule,
+    MatTooltipModule,
+    MatIconModule,
+    MatSnackBarModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatCardModule
   ],
   declarations: [
     ListagemComponent, 
